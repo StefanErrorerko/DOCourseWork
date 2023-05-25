e060ec8991fb4df4c401dd1c89c65f42f6a26d5d
Stefan Muzyka <nashes1320@gmail.com>
2023-05-25 23:21:44 +0300

    final commit for algo 1

--- main.py
+++ main.py
@@ -1,9 +1,18 @@
-import decompose_solver
-import problem_generator
+from tabulate import tabulate
 
-n, m, T = 18, 2, {'start': 10, 'end': 16}
+import experiment_generator
+from util import print_results
 
-C, p, w, ab, cd = problem_generator.generate_problem(n, T)
-S, z1 = decompose_solver.solve(C, w, m, p, T, ab, cd)
-print(S)
-print(z1)
+N = [18, 25, 30, 50, 100]
+M = [1, 2, 3, 4, 5]
+T_arr = [{'start': 10, 'end': 16},
+         {'start': 10, 'end': 17},
+         {'start': 10, 'end': 18},
+         {'start': 9, 'end': 18},
+         {'start': 9, 'end': 19}]
+
+dT, dZ = experiment_generator.with_n(N=N, m=2, T=T_arr[0])
+print_results(N, dT, dZ, 'N')
+dT, dZ = experiment_generator.with_m(n=50, M=M, T=T_arr[0])
+dT, dZ = experiment_generator.with_T(n=50, m=2, T=T_arr)
+
--- util.py
+++ util.py
@@ -1,4 +1,5 @@
 import numpy as np
+from tabulate import tabulate
 
 
 def rand_normal(mean, start, end):
@@ -11,3 +12,46 @@ def rand_exp(l, start, end):
     rand = int(round(
         (end - start) * np.clip(1/np.random.exponential(1/l), 0, 1)))
     return start + rand
+
+
+def bubble_sort_wt(arr, w, ab, cd):
+    n = len(arr)
+    for i in range(n - 1):
+        for j in range(0, n - i - 1):
+            t1_1, t2_1 = interval_len(ab[j]), interval_len(cd[j])
+            t1_2, t2_2 = interval_len(ab[j+1]), interval_len(cd[j+1])
+            if w[j]/(t1_1 + t2_1) < w[j+1]/(t1_2 + t2_2):
+                arr[j], arr[j + 1] = arr[j + 1], arr[j]
+
+
+def interval_len(interval):
+    return interval['end'] - interval['start']
+
+
+# whether subset belongs to a set
+def belongs(subset, set):
+    if subset['start'] >= set['start'] and subset['end'] <= set['end']:
+        return True
+    return False
+
+
+def print_results(observable_values, dT, dZ, nameof_value):
+    header_column = [nameof_value,
+                     'dT Algo 1', 'dT Algo 2', 'dT Algo 3'
+                     'dZ Algo 1', 'dZ Algo 2', 'dZ Algo 3']
+    dT = reshape(dT, 3, len(dT))
+    dZ = reshape(dZ, 3, len(dZ))
+
+    table_data = list(zip(observable_values,
+                          dT[0], dT[1], dT[2],
+                          dZ[0], dZ[1], dZ[2]))
+    table = tabulate(table_data, headers=header_column, tablefmt="grid")
+    print(table)
+
+def reshape(arr, r, c):
+    new_arr = []
+    for i in range(r):
+        new_arr.append([])
+        for j in range(c):
+            new_arr[i].append(arr[j][i])
+    return  new_arr
