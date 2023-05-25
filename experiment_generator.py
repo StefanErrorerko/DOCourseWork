0eeac0ede4dd84c189b27cdf070f48abd3f519fd
Stefan Muzyka <nashes1320@gmail.com>
2023-05-26 01:09:35 +0300

    zdelali time algoritm

--- experiment_generator.py
+++ experiment_generator.py
@@ -1,3 +1,4 @@
+import combine_solver
 import decompose_solver
 import problem_generator
 import time
@@ -44,20 +45,23 @@ def test_problem(n, m, T):
         t1.append(end_time - start_time)
 
         # solving with 2 algorythm, computing time t2 and z2
-        # place for same stuff for 2 algo
+        start_time = time.time()
+        S, z2 = combine_solver.solve(C, w, m, p, T, ab, cd)
+        end_time = time.time()
+        t2.append(end_time - start_time)
 
         # solving with 3 algorythm, computing time t3 and z3
         # place for same stuff for 2 algo
 
         #z_mean = (z1 + z2 + z3) / 3
-        z_mean = z1
+        z_mean = (z1 + z2) / 2
         dz1.append((z1 - z_mean) / z_mean)
-        #dz2.append((z2 - z_mean) / z_mean)
+        dz2.append((z2 - z_mean) / z_mean)
         #dz3.append((z3 - z_mean) / z_mean)
     # t1, t2, t3 = sum(t1)/len(t1), sum(t2)/len(t2), sum(t3)/len(t3)
-    t1, t2, t3 = sum(t1) / len(t1), 0, 0
+    t1, t2, t3 = sum(t1) / len(t1), sum(t2) / len(t2), 0
     # dz1, dz2, dz3 = sum(dz1)/len(dz1), sum(dz2)/len(dz2), sum(dz3)/len(dz3)
-    dz1, dz2, dz3 = sum(dz1) / len(dz1), 0, 0
+    dz1, dz2, dz3 = sum(dz1) / len(dz1), sum(dz2)/len(dz2), 0
     return [t1, t2, t3], [dz1, dz2, dz3]
 
 
--- util.py
+++ util.py
@@ -24,6 +24,26 @@ def bubble_sort_wt(arr, w, ab, cd):
                 arr[j], arr[j + 1] = arr[j + 1], arr[j]
 
 
+def bubble_sort_t(arr, w, ab, cd):
+    n = len(arr)
+    for i in range(n - 1):
+        for j in range(0, n - i - 1):
+            t1_1, t2_1 = interval_len(ab[j]), interval_len(cd[j])
+            t1_2, t2_2 = interval_len(ab[j + 1]), interval_len(cd[j + 1])
+            if (t1_1 + t2_1) > (t1_2 + t2_2):
+                arr[j], arr[j + 1] = arr[j + 1], arr[j]
+
+
+def bubble_sort_tw(arr, w, ab, cd):
+    n = len(arr)
+    for i in range(n - 1):
+        for j in range(0, n - i - 1):
+            t1_1, t2_1 = interval_len(ab[j]), interval_len(cd[j])
+            t1_2, t2_2 = interval_len(ab[j + 1]), interval_len(cd[j + 1])
+            if (t1_1 + t2_1) / w[j] < (t1_2 + t2_2) / w[j + 1]:
+                arr[j], arr[j + 1] = arr[j + 1], arr[j]
+
+
 def interval_len(interval):
     return interval['end'] - interval['start']
 
